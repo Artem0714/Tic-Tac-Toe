78ee6cb7431f0a573b0d0a085d99d98c907f7207
Artem0714 <arfed96@gmail.com>
2022-09-01 17:18:11 +0300

    InputInJS

--- options.js
+++ options.js
@@ -1,5 +1,28 @@
-document.querySelector('#elem').onclick = function getValue() {
-    elem.value = '×';
-    // elem.value = '○';
-    document.querySelector('#elem').style.color = 'black';
+for (let i = 0; i < 9; i++) {
+    document.body.insertAdjacentHTML('beforeend', '<input id = "elem" type = "button">  </input>');
 }
+
+const buttons = document.querySelectorAll('input')
+const orderMove = {
+    motion: 0
+};
+const bumButton = document.querySelectorAll('#num')
+
+
+buttons.forEach(elem => {
+    elem.addEventListener('click', function getValue() {
+        orderMove.motion += 1;
+        if (orderMove.motion > 9) {
+            alert('Игра закончена!')
+        } else if (orderMove.motion % 2 === 0) {
+            elem.value = '○';
+        } else {
+            elem.value = '×';
+        };
+        console.log(bumButton)
+        // if (orderMove.motion > 6) {
+        //     if ()
+        // }
+        // document.querySelector('#elem').style.color = 'black';
+    });
+});
